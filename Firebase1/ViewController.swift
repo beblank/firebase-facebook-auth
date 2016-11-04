c0ff8638d4a397596ca87dfd360d1af2be4655be
beblank <kulik2@ymail.com>
2016-11-04 13:51:56 +0700

    stable facebook login and save to firebase sign in auth

--- Firebase1/ViewController.swift
+++ Firebase1/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    @IBAction func LoginFacebookTaped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler: {(result, error) in
+            if error != nil{
+                print("Dodol, unable to auth with facebook \(error)")
+            } else if result?.isCancelled == true{
+                print("Dodol: cancel auth with facebook")
+            } else {
+                print("Dodol: auth with facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        })
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: {(user, error) in
+            if error != nil {
+                print("Dodol: unable to auth with firebase \(error)")
+            } else {
+                print("Dodol: auth with firebase")
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
